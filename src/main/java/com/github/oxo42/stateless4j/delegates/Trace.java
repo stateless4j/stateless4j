a41501249dd172916823d94667f2bfab72cf9714
Chris Narkiewicz <hello@ezaquarii.com>
2019-09-13 12:57:23 +0100

    Trace delegate JavaDoc
    Signed-off-by: Chris Narkiewicz <hello@ezaquarii.com>

--- src/main/java/com/github/oxo42/stateless4j/delegates/Trace.java
+++ src/main/java/com/github/oxo42/stateless4j/delegates/Trace.java
@@ -1,6 +1,29 @@
 package com.github.oxo42.stateless4j.delegates;
 
+/**
+ * Tracing delegate allows one to investigate state machine working at runtime.
+ *
+ * @see com.github.oxo42.stateless4j.StateMachine#setTrace(Trace)
+ * 
+ * @param <S> State type
+ * @param <T> Trigger type
+ */
 public interface Trace<S, T> {
+
+    /**
+     * This callback is called each time a trigger is fired, before evaluation,
+     * allowing to trace all events sent to the sate machine
+     *
+     * @param trigger Trigger sent to the state machine
+     */
     void trigger(T trigger);
+
+    /**
+     * This callback is called each time a transition is performed, after trigger evaluation.
+     *
+     * @param trigger Trigger sent to the state machine
+     * @param source Source state
+     * @param destination Destination state
+     */
     void transition(T trigger, S source, S destination);
 }
